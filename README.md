e394bded41b6db5e236a858a9605a2b7bf2ab54a
Marco Michelino <marco.michelino@immobiliare.it>
2024-11-30 06:42:41 +0100

    doc + allow the airdropper talk directly to the locker

--- README.md
+++ README.md
@@ -1,1 +1,95 @@
 # Radical Doge rewards
+
+This blueprint allows the owner to lock a fungible coin and to distribute it later as a reward. Only the owner can deposit the rewards, he can do it multiple times.  
+The holder of the airdropper badge can input an IndexMap (Account => Amount) to distribute part of the rewards previously deposited by the owner.  
+The distribution happens through an AccountLocker, the airdropper is also allowed to interact directly with the AccountLocker to distribute any different reward than the one deposited in this component.  
+
+## Known bugs and limitations
+
+It is not possible to distribute rewards to more than about 100 recipients in a single transaction due to Radix network transaction limits.  
+
+## Transaction manifests
+
+### new
+
+Instantiate a globalized RadicalDogeRewards component.  
+Looking at the transaction details you will see that a `locker_...` component has been created too.  
+
+```
+CALL_FUNCTION
+    Address("")
+    "RadicalDogeRewards"
+    "new"
+    Address("<OWNER_BADGE_ADDRESS>")
+    Address("<AIRDROPPER_BADGE_ADDRESS>")
+    Address("<REWARDS_ADDRESS>")
+;
+```
+
+`<OWNER_BADGE_ADDRESS>` Resource address of the owner badge.  
+`<AIRDROPPER_BADGE_ADDRESS>` Resource address of the airdropper badge.  
+`<REWARDS_ADDRESS>` Resource address of the coin that will be distributed as a reward.  
+
+### deposit_future_rewards
+
+Lock future rewards in the component. Only the owner is allowed call this method.  
+
+```
+CALL_METHOD
+    Address("<ACCOUNT>")
+    "create_proof_of_amount"
+    Address("<OWNER_BADGE_ADDRESS>")
+    Decimal("1")
+;
+CALL_METHOD
+    Address("<ACCOUNT>")
+    "withdraw"
+    Address("<REWARDS_ADDRESS>")
+    Decimal("<REWARDS_AMOUNT>")
+;
+TAKE_ALL_FROM_WORKTOP
+    Address("<REWARDS_ADDRESS>")
+    Bucket("future_rewards")
+;
+CALL_METHOD
+    Address("<DOGE_REWARDS_COMPONENT_ADDRESS>")
+    "deposit_future_rewards"
+    Bucket("future_rewards")
+;
+```
+
+`<ACCOUNT>` The account holding the owner badge and the rewards to deposit.  
+`<OWNER_BADGE_ADDRESS>` Resource address of the owner badge (I assumed it is a fungible resource).  
+`<REWARDS_ADDRESS>` Resource address of the coin that will be distributed as a reward.  
+`<REWARDS_AMOUNT>` The amount of rewards to deposit.  
+`<DOGE_REWARDS_COMPONENT_ADDRESS>` The address of the RadicalDogeRewards component.  
+
+### distribute_deposited_rewards
+
+Distribute previously deposited rewards using an AccountLocker. Only the airdropper is allowed to call this method.  
+In order to build the list of recipents I suggest to use [this API](https://radix-babylon-gateway-api.redoc.ly/#operation/ResourceHoldersPage) to get the holders of the LP token. In case of DOGE the OciSwap LP token is `resource_rdx1t47kxqvrzfj8vl4f2tpm0d9fy8gjt74nhxn52g33tu6urlgh0y3kuf`.  
+It is also possible to schedule this operation without human interaction by creating a bot that uses a [typescript radix wallet](https://github.com/radixdlt/experimental-examples/tree/main/typescript-wallet) to execute the transactions. The account just needs the airdropper badge and some XRD to pay the fees.  
+
+```
+CALL_METHOD
+	Address("<ACCOUNT>")
+	"create_proof_of_amount"
+	Address("<AIRDROPPER_BADGE_ADDRESS>")
+	Decimal("1")
+;
+CALL_METHOD
+	Address("<DOGE_REWARDS_COMPONENT_ADDRESS>`")
+	"distribute_deposited_rewards"
+	Map<Address, Enum>(
+        Address("<RECIPIENT>") => Enum<0u8>(Decimal("<AMOUNT>")),
+        ...
+    )
+;
+```
+
+`<ACCOUNT>` The account holding the airdroppper badge.  
+`<AIRDROPPER_BADGE_ADDRESS>` Resource address of the airdropper badge (I assumed it is a fungible resource).  
+`<DOGE_REWARDS_COMPONENT_ADDRESS>` The address of the RadicalDogeRewards component.  
+`<RECIPIENT>` The account address of one of the recipents of the airdrop.  
+`<AMOUNT>` The amount of rewards for the recipent.  
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,17 @@
 use scrypto::prelude::*;
 
+/* This blueprint allows the owner to lock a fungible coin and to distribute it later as a reward.
+ *
+ * Only the owner can deposit the rewards, he can do it multiple times.
+ *
+ * The holder of the airdropper badge can input an IndexMap (Account => Amount) to distribute part
+ * of the rewards previously deposited by the owner.
+ *
+ * The distribution happens through an AccountLocker, the airdropper is also allowed to interact
+ * directly with the AccountLocker to distribute any different reward than the one deposited
+ * in this component.
+*/
+
 #[blueprint]
 mod radical_doge_rewards {
 
@@ -9,18 +21,22 @@ mod radical_doge_rewards {
         },
         methods {
             deposit_future_rewards => restrict_to: [OWNER];
-            assign_deposited_rewards => restrict_to: [airdropper];
-            assign_rewards => restrict_to: [airdropper];
+            distribute_deposited_rewards => restrict_to: [airdropper];
         }
     }
 
     struct RadicalDogeRewards {
+
+        // The vault to contain the future rewards
         future_rewards: FungibleVault,
+
+        // The AccountLocker to distribute the rewards
         account_locker: Global<AccountLocker>,
     }
 
     impl RadicalDogeRewards {
 
+        // Instantiate a globalized RadicalDogeRewards component
         pub fn new(
             owner_badge_address: ResourceAddress,
             airdropper_badge_address: ResourceAddress,
@@ -31,16 +47,25 @@ mod radical_doge_rewards {
             let (address_reservation, component_address) =
                 Runtime::allocate_component_address(RadicalDogeRewards::blueprint_id());
 
+            // Both this component and the holder of the airdropper_badge are authorized to deposit in the AccountLocker
+            let storer_role = rule!(require(CompositeRequirement::AnyOf(vec![
+                    global_caller(component_address).into(),
+                    require(airdropper_badge_address),
+            ])));
+
+            // Instantiate an AccountLocker to distribute rewards
+            let account_locker = Blueprint::<AccountLocker>::instantiate(
+                OwnerRole::Updatable(rule!(require(owner_badge_address))), // owner_role
+                storer_role,
+                rule!(require(owner_badge_address)), // storer_updater_role
+                rule!(deny_all), // recoverer_role
+                rule!(require(owner_badge_address)), // recoverer_updater_role
+                None
+            );
+
             Self {
                 future_rewards: FungibleVault::new(rewards_address),
-                account_locker: Blueprint::<AccountLocker>::instantiate(
-                    OwnerRole::Updatable(rule!(require(owner_badge_address))),
-                    rule!(require(global_caller(component_address))),
-                    rule!(require(owner_badge_address)),
-                    rule!(deny_all),
-                    rule!(require(owner_badge_address)),
-                    None
-                ),
+                account_locker: account_locker,
             }
             .instantiate()
             .prepare_to_globalize(OwnerRole::Updatable(rule!(require(owner_badge_address))))
@@ -51,6 +76,7 @@ mod radical_doge_rewards {
             .globalize()
         }
 
+        // Lock future rewards in the component
         pub fn deposit_future_rewards(
             &mut self,
             future_rewards: FungibleBucket,
@@ -58,12 +84,16 @@ mod radical_doge_rewards {
             self.future_rewards.put(future_rewards);
         }
 
-        pub fn assign_deposited_rewards(
+        // Distribute previously deposited rewards using an AccountLocker
+        pub fn distribute_deposited_rewards(
             &mut self,
+
+            // Account => ResourceSpecifier::Fungible(Decimal)
             rewards: IndexMap<Global<Account>, ResourceSpecifier>,
         ) {
-            let mut total_coins = Decimal::ZERO;
 
+            // Compute the total amount to distribute
+            let mut total_coins = Decimal::ZERO;
             for resource_specifier in rewards.values() {
                 match resource_specifier {
                     ResourceSpecifier::Fungible(amount) => total_coins += *amount,
@@ -71,33 +101,24 @@ mod radical_doge_rewards {
                 }
             }
 
+            // Take all of the rewards to distribute out of the vault
             let rewards_bucket = self.future_rewards.take_advanced(
                 total_coins,
                 WithdrawStrategy::Rounded(RoundingMode::AwayFromZero)
             );
 
+            // Distribute rewards and get back any remaining coin due to divisibility rounding
             let remainings = self.account_locker.airdrop(
                 rewards,
                 rewards_bucket.into(),
                 true
             );
 
+            // If there are remaining coins put them back into the vault
             match remainings {
                 Some(bucket) => self.future_rewards.put(FungibleBucket(bucket)),
                 None => {},
             }
-        }
-
-        pub fn assign_rewards(
-            &mut self,
-            rewards: IndexMap<Global<Account>, ResourceSpecifier>,
-            rewards_bucket: Bucket,
-        ) -> Option<Bucket> {
-            self.account_locker.airdrop(
-                rewards,
-                rewards_bucket,
-                true
-            )
         }
     }
 }
